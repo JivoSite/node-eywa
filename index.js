844a12e0f77cb68029c7e64e77e4b52d9c8dcd67
Yevgeny Mikhaylov <mikhaylov@jivosite.com>
2017-01-02 13:04:59 +0700

    delete neuron log if launch axon
    Signed-off-by: Yevgeny Mikhaylov <mikhaylov@jivosite.com>

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ else
          , '/etc/node-eywa.json', '~/.node-eywa.json');
       log.verbose = conf.verbose;
       log.add(conf.log);
+      if (!conf.neuron && conf.log && conf.log.neuron) delete conf.log.neuron;
       (require(conf.neuron ? './lib/neuron' : './lib/axon-shell')
       )(conf).open(args[args.length - 1]);
    }
